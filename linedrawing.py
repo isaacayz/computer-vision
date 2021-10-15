debb6192fc605bd55a43beabf2d4c5324413616e
Isaac Ige <heywhydot16@gmail.com>
2021-10-15 15:29:04 +0100

    drawing rectangle, circle, putText and font***

--- linedrawing.py
+++ linedrawing.py
@@ -10,6 +10,10 @@ while True:
 
     img = cv2.line(frame, (0, 0), (width, height), (255, 0, 0), 2)
     img = cv2.line(img, (0, height), (width, 0), (0, 255, 0), 2)
+    img = cv2.rectangle(img, (100,100), (300, 300), (0,255,60), 4)
+    img = cv2.circle(img, (200, 200), 60, (0,0,255), 9)
+    font = cv2.FONT_HERSHEY_COMPLEX
+    img = cv2.putText(img, 'Isaac is great', (200, height - 20), font, 2, (0, 128, 128), 5, cv2.LINE_AA)
 
     cv2.imshow('Frame', img)
     if cv2.waitKey(1) == ord('q'):
