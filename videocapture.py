5625ff1e3eb52dda592ecd4c34386ad8f7a798e9
Isaac Ige <heywhydot16@gmail.com>
2021-10-15 11:44:55 +0100

    video capture draft**

---
+++ videocapture.py
@@ -0,0 +1,13 @@
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    cv2.imshow('Frame', frame)
+    if cv2.waitKey(1) == ord('x'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
