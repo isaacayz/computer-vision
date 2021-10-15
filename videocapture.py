adfc37d011e811bcd0eafbfdc0561a966a0a06b2
Isaac Ige <heywhydot16@gmail.com>
2021-10-15 14:08:16 +0100

    split the images into 4 and rotated the top two in 180 degrees***

--- videocapture.py
+++ videocapture.py
@@ -7,12 +7,20 @@ while True:
     ret, frame = cap.read()
     width = int(cap.get(3))
     height = int(cap.get(4))
-    
+
     image = np.zeros(frame.shape, np.uint8)
     smaller_frame = cv2.resize(frame, (0,0), fx=0.5, fy=0.5)
+    #paste the image in the top left frame
+    image[:height//2, :width//2] = cv2.rotate(smaller_frame,cv2.ROTATE_180)
+    #paste the image in the bottom left frame
+    image[height//2:, :width//2] = smaller_frame
+    #paste the image in the top right frame
+    image[:height//2, width//2:] = cv2.rotate(smaller_frame,cv2.ROTATE_180)
+    #paste the image in the bottom right frame
+    image[height//2:, width//2:] = smaller_frame
 
 
-    cv2.imshow('Frame', smaller_frame)
+    cv2.imshow('Frame', image)
     if cv2.waitKey(1) == ord('x'):
         break
 
