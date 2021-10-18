59a803e0a50253fe744fa52eeaaf4da4daab5827
Isaac Ige <heywhydot16@gmail.com>
2021-10-18 17:47:59 +0100

    working facial detection with Isaac and Trump

--- flask-project/main.py
+++ flask-project/main.py
@@ -15,21 +15,21 @@ import numpy as np
 video_capture = cv2.VideoCapture(0)
 
 # Load a sample picture and learn how to recognize it.
-obama_image = face_recognition.load_image_file("obama.jpg")
-obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
+isaac_image = face_recognition.load_image_file("flask-project/Isaac/isaac.jpg",mode='RGB') #('Isaac/isaac.jpg')
+isaac_face_encoding = face_recognition.face_encodings(isaac_image)[0]
 
 # Load a second sample picture and learn how to recognize it.
-biden_image = face_recognition.load_image_file("biden.jpg")
-biden_face_encoding = face_recognition.face_encodings(biden_image)[0]
+trump_image = face_recognition.load_image_file("flask-project/Trump/trump.jpg")
+trump_face_encoding = face_recognition.face_encodings(trump_image)[0]
 
 # Create arrays of known face encodings and their names
 known_face_encodings = [
-    obama_face_encoding,
-    biden_face_encoding
+    isaac_face_encoding,
+    trump_face_encoding
 ]
 known_face_names = [
-    "Barack Obama",
-    "Joe Biden"
+    "Daddy Isaac",
+    "Donald Trump"
 ]
 
 # Initialize some variables
